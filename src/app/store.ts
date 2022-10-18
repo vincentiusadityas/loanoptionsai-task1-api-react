fa785fafca90fd7478cb62cad83a1a675c1aa3cb
Vincentius Aditya Sundjaja <vasundjaja@gmail.com>
2022-10-18 23:32:29 +0700

    removed unused code created from template

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import tableReducer from '../features/table/tableSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     table: tableReducer
   },
 });
